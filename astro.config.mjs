3a1d78c4ae5e923e921c780bf3d5d0e6efddbd75
Riley <riley@rileybathurst.com>
2024-07-18 15:36:35 -0700

    rates page and I deleted the storyboard because astro had a hissy fit and its not quite as good as I wanted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
-import db from "@astrojs/db";
+// import db from "@astrojs/db";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  /*
+  ? something is broken try clean it up
   vite: {
-    css: {
-      transformer: "lightningcss"
-    }
-  },
+      css: {
+        transformer: "lightningcss"
+      }
+    }, */
   integrations: [react()]
 });
--- src/interfaces/about-types.ts
+++ src/interfaces/about-types.ts
@@ -1,4 +1,4 @@
-import type { BlocksContent } from "@strapi/blocks-react-renderer";
+// import type { BlocksContent } from "@strapi/blocks-react-renderer";
 import type StrapiImagesTypes from "./strapi-images-types";
 
 export default interface AboutTypes {
