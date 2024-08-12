6bda99062ae83f8b13eb24a89f4faad4bb972b6f
Riley <riley@rileybathurst.com>
2024-08-11 17:19:30 -0700

    remove vrbo

--- src/interfaces/rate-types.ts
+++ src/interfaces/rate-types.ts
@@ -3,7 +3,6 @@ import type { BlocksContent } from "@strapi/blocks-react-renderer";
 export default interface RateTypes {
   attributes: {
     taxId: string;
-    vrbo: string;
     children: BlocksContent;
     cleaning: BlocksContent;
     direct: BlocksContent;
